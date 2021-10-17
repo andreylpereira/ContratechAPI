a3e6885392ad11b5482d6085a15d393a67565af1
andreylpereira <andreylpereira@gmail.com>
2021-10-17 11:25:40 -0300

    correção de arquivo com erro

--- src/main/java/com/senai/contratech/controllers/ServicoController.java
+++ src/main/java/com/senai/contratech/controllers/ServicoController.java
@@ -45,11 +45,11 @@ public class ServicoController {
 	
 	
 	//atualizar
-	@PutMapping("/usuarios/{usuarioId}/obras/{obraId}/etapas/{etapaId}/servicos/atualizar")
-	public void atualizarTodosServicos(Long usuarioId, @PathVariable Long obraId, @PathVariable Long etapaId, @RequestBody List<Servico> servicos) {
-		//servicoService.putAllServicos(usuarioId, obraId, etapaId, servicos);
-
-	}
+//	@PutMapping("/usuarios/{usuarioId}/obras/{obraId}/etapas/{etapaId}/servicos/atualizar")
+//	public void atualizarTodosServicos(Long usuarioId, @PathVariable Long obraId, @PathVariable Long etapaId, @RequestBody List<Servico> servicos) {
+//		servicoService.putAllServicos(usuarioId, obraId, etapaId, servicos);
+//
+//	}
 	
 }
 
