ed2a79a160de88d18f3f376abed63ec3171fdf38
andreylpereira <andreylpereira@gmail.com>
2021-11-04 00:06:55 -0300

    Dto informando id, login e token do usuario

--- src/main/java/com/senai/contratech/controllers/SegurancaController.java
+++ src/main/java/com/senai/contratech/controllers/SegurancaController.java
@@ -1,5 +1,7 @@
 package com.senai.contratech.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.senai.contratech.model.usuario.dto.DtoToken;
 import com.senai.contratech.model.usuario.entity.Usuario;
+import com.senai.contratech.model.usuario.repository.UsuarioRepository;
 import com.senai.contratech.model.usuario.service.AutenticacaoUsuarioService;
 import com.senai.contratech.seguranca.JWTHelper;
 
@@ -21,20 +25,36 @@ public class SegurancaController {
 
 	@Autowired
 	private AutenticacaoUsuarioService autenticacaoUsuarioService;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@Autowired
 	private JWTHelper jwtHelper;
 
 	@PostMapping("/login")
-	public ResponseEntity<String> buscarToken(@RequestBody Usuario usuario) {
+	public ResponseEntity<DtoToken> buscarToken(@RequestBody Usuario usuario) {
+		
 		try {
 			UserDetails detalhesUsuario = autenticacaoUsuarioService.loadUserByUsername(usuario.getLogin());
 			if (!new BCryptPasswordEncoder().matches(usuario.getSenha(), detalhesUsuario.getPassword())) {
-				return new ResponseEntity<>(jwtHelper.gerarToken(detalhesUsuario), HttpStatus.UNAUTHORIZED);
+				Optional<Usuario> user = usuarioRepository.findByLogin(usuario.getLogin());
+				DtoToken token = new DtoToken();
+				token.setId(user.get().getId());
+				token.setLogin(usuario.getLogin());
+				token.setToken(jwtHelper.gerarToken(detalhesUsuario));
+				return new ResponseEntity<>(token, HttpStatus.UNAUTHORIZED);
 			}
-			return new ResponseEntity<>(jwtHelper.gerarToken(detalhesUsuario), HttpStatus.OK);
+			
+			Optional<Usuario> user = usuarioRepository.findByLogin(usuario.getLogin());
+			DtoToken token = new DtoToken();
+			token.setId(user.get().getId());
+			token.setLogin(usuario.getLogin());
+			token.setToken(jwtHelper.gerarToken(detalhesUsuario));
+			//jwtHelper.gerarToken(detalhesUsuario)
+			
+			return new ResponseEntity<>(token, HttpStatus.OK);
 		} catch (UsernameNotFoundException e) {
-			return new ResponseEntity<>("Usuário Inválido", HttpStatus.UNAUTHORIZED);
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}
 	}
 }
